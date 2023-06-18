fdb061afc480fd34dc841a4356aa115a0d5bf1ac
Vitor do Nascimento <45907317+vito0182@users.noreply.github.com>
2023-06-18 13:26:36 -0300

    Add files via upload

---
+++ scrapper.py
@@ -0,0 +1,68 @@
+from bs4 import BeautifulSoup
+from urllib.request import urlopen
+import re
+import csv
+import copy
+
+url = "http://www.stellar-database.com/Scripts/search_star.exe?ID=200"
+page = urlopen(url)
+
+html_bytes = page.read()
+html = html_bytes.decode("ISO-8859-1")
+
+soup = BeautifulSoup(html, "html.parser")
+
+text = soup.get_text()
+
+def content_getter(column):
+    pattern = re.compile(fr"{column} (.*)")
+    matches = re.findall(pattern, text)
+    if matches:
+        content = matches[0]
+    else:
+        content = ""
+    return content
+
+columns = ["Distance from Sol:", "Diameter:", "Luminosity Class:", "Apparent visual magnitude:", "Absolute visual magnitude:", "Visual luminosity:", "Color indices:"]
+dict_original = {"star_name": None}
+
+for column in columns:
+        dict_original[column] = None
+
+
+myFile = open('Demo.csv', 'r+')
+
+id = 100
+
+while True:
+    
+    dict_star = copy.deepcopy(dict_original)
+
+    url = f"http://www.stellar-database.com/Scripts/search_star.exe?ID={id}"
+    page = urlopen(url)
+    html_bytes = page.read()
+    html = html_bytes.decode("ISO-8859-1")
+
+    soup = BeautifulSoup(html, "html.parser")
+
+    text = soup.get_text()
+    
+    for column in columns:
+        dict_star[column] = content_getter(column)
+
+    first_h1 = soup.find('h1')
+
+    if first_h1:
+        h1_content = first_h1.text
+        dict_star["star_name"] = h1_content
+    else:
+        print("No 'h1' tag found.")
+        break
+
+    id += 100
+    true_id = id / 100
+
+    writer = csv.writer(myFile)
+    writer.writerow(dict_star.values())
+    print(true_id)
+    dict_star = {}
